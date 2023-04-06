fb635b4c7c5c3bf4cc93818e46bf7a49d4e92d6c
dongdong2 <ehdwngudwn@naver.com>
2023-04-06 23:19:43 +0900

    Modify Lobby

--- Assets/Scripts/UI/LobbyUI.cs
+++ Assets/Scripts/UI/LobbyUI.cs
@@ -22,6 +22,7 @@ public class LobbyUI : MonoBehaviour
     Image progressBar;
 
 
+
     public void SetState(int state)
     {
         this.state = (LobbyState)state;
@@ -52,7 +53,10 @@ public class LobbyUI : MonoBehaviour
 
     private void UpdateReady()
     {
-
+        if(Managers.Game.NextStage != Define.GameSceneOrder.TimeScene_main)
+        {
+            progressBar.fillAmount = (float)(Managers.Game.NextStage - 1) / (float)Define.GameSceneOrder.Count;    
+        }
     }
     private void UpdateScan()
     {
@@ -61,6 +65,7 @@ public class LobbyUI : MonoBehaviour
     }
     private void UpdateScanEnd()
     {
+        progressBar.color = Color.red;
 
     }
     private void UpdateFinish()
@@ -72,9 +77,23 @@ public class LobbyUI : MonoBehaviour
     private void UpdateProgress()
     {
         float progress = (float)Managers.Game.NextStage /  (float)Define.GameSceneOrder.Count;
+        if(progressBar.fillAmount == 1)
+        {
+            //End!!
+            Debug.Log("end");
+            state = LobbyState.Finish;
+        }
         if(progressBar.fillAmount <= progress)
         {
             progressBar.fillAmount += 0.1f * Time.deltaTime;
+
+        }
+        else
+        {
+            state = LobbyState.ScanEnd;
+            Util.FindChild(gameObject, $"{Managers.Game.NextStage.ToString()}_Btn", true).GetComponent<Animator>().enabled = true;
+            GetComponent<Animator>().SetBool("ScanEnd", true);
+
         }
     }
 }
